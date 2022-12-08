9ccadb8f6ae9006c6c89bb6e318b4dca586afbfe
nitya-cs <nitya.agarwala@gmail.com>
2022-12-08 23:15:02 +0530

    added duration input

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,22 @@ import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 import Invites from './components/Invites';
 import axios from 'axios';
+import NavigationBar from './components/NavigationBar';
+import Header from './components/Header';
 
 function App() {
 
   const [name, setName] = useState("");
+  const [duration, setDuration] = useState(60);
   const [disable, setDisable] = useState(false);
   const [eventData, setEventData] = useState([]);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
+  };
+
+  const handleDurationChange = (event) => {
+    setDuration(event.target.value);
   };
 
   const months = ["January", "February", "March", "April", "May", "June",
@@ -41,12 +48,12 @@ function App() {
     return (
       <div style={{ lineHeight: 2.5, paddingTop: 10 }}>
         <div> {day + " " + month + ", " + year + " at " + startTime + " - " + endTime + " hours"}</div>
-        <div> <span style={{fontWeight: 600}}>Title: </span>{eventData.summary} </div>
-        <div style={{fontWeight: 600}}> {"Attendees:"} </div>
+        <div> <span style={{ fontWeight: 600 }}>Title: </span>{eventData.summary} </div>
+        <div style={{ fontWeight: 600 }}> {"Attendees:"} </div>
         <ul>
           {eventData.attendees.map((attendee) => returnAttendeeInfo(attendee))}
         </ul>
-        <div> <span style={{fontWeight: 600}}>View Event: </span> <a href={eventData.htmlLink} target="_blank" rel="noreferrer">at this link</a></div>
+        <div> <span style={{ fontWeight: 600 }}>View Event: </span> <a href={eventData.htmlLink} target="_blank" rel="noreferrer">at this link</a></div>
       </div>
     );
   };
@@ -54,10 +61,11 @@ function App() {
   const sendRequest = async (emails) => {
     setDisable(true);
     // send request to backend with name and emails
-    console.log(name);
-    console.log(emails);
+    // console.log(name);
+    // console.log(emails);
     const createEventObject = {
       eventName: name,
+      eventDuration: duration,
       calendarIds: [...emails, 'primary'],
       emailAddresses: emails,
     };
@@ -71,44 +79,49 @@ function App() {
   }
 
   return (
-    <Container>
-      <div style={styles.heading}>Cal-Schedule</div>
-      <Row>
-        <Col sm style={{ padding: 10 }}>
-          <div style={styles.subtitle}>What are you trying to plan?</div>
-          <div>Event Name: </div>
-          <input type="text" value={name} onChange={handleNameChange} disabled={disable} />
-          {/* Component to add people's names will go here! */}
-          <div style={{ height: 50 }}></div>
-          <div style={styles.subtitle}>Who do you want to invite?</div>
-          <div>Attendees: </div>
-          <Invites disabled={disable} createEvent={sendRequest}></Invites>
-        </Col>
+    <>
+      <NavigationBar></NavigationBar>
+      <Container style={styles.container}>
+        <Header></Header>
+        <Row>
+          <Col sm style={{ padding: 10 }}>
+            <div style={styles.subtitle}>What are you trying to plan?</div>
+            <div>Event Name: </div>
+            <input type="text" value={name} onChange={handleNameChange} disabled={disable} />
+            <div style={{ height: 20 }}></div>
+            <div>Duration (minutes): </div>
+            <input type="number" value={duration} onChange={handleDurationChange} disabled={disable} />
+            {/* Component to add people's names will go here! */}
+            <div style={{ height: 30 }}></div>
+            <div style={styles.subtitle}>Who do you want to invite?</div>
+            <div>Attendees: </div>
+            <Invites disabled={disable} createEvent={sendRequest}></Invites>
+          </Col>
 
-        {/* Right column displays event */}
+          {/* Right column displays event */}
 
-        <Col sm style={{ padding: 10, marginLeft: '5%' }}>
-          <div>Your Event has been scheduled for:</div>
-          {renderEventData()}
-        </Col>
-      </Row>
+          <Col sm style={{ padding: 10, marginLeft: '5%' }}>
+            <div>Your Event has been scheduled for:</div>
+            {renderEventData()}
+          </Col>
+        </Row>
 
-      {/* create new event button */}
-      <button style={{ float: 'right' }} onClick={() => { setEventData([]); setName(""); setDisable(false) }}>
-        Create Another Event
-      </button>
-    </Container>
+        {/* create new event button */}
+        <button style={{ float: 'right',  borderRadius: 5, }} onClick={() => { setEventData([]); setName(""); setDisable(false) }}>
+          Create Another Event
+        </button>
+      </Container>
+    </>
   );
 }
 
 const styles = {
-  heading: {
-    textAlign: 'center',
-    padding: 20,
-    fontSize: 30,
+  container:{
   },
   subtitle: {
     fontStyle: 'italic',
+    fontSize: 18,
+    marginBottom: 10,
   }
 };
 
